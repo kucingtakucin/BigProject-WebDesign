ae0d58e295113adc71c5454c58a6f7fe0375840e
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-05-02 19:20:27 +0700

    Baru bikin navbar wkwk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useState} from 'react';
+import PropTypes from 'prop-types';
+import {Route, Switch} from "react-router-dom";
+import {
+    Collapse,
+    Container,
+    Jumbotron,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavbarText,
+    NavbarToggler,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+import './sass/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyApp extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <AppNavbar/>
+                <AppHeader/>
+                <AppMain/>
+                <AppFooter/>
+            </React.Fragment>
+        );
+    }
 }
 
-export default App;
+function AppNavbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    return (
+        <Navbar color="info" dark={true} fixed={true} expand="md" className="App-nav">
+            <Container fluid={true} className="text-center">
+                <NavbarBrand href="/" className="font-weight-bold">Labkom</NavbarBrand>
+                <NavbarToggler onClick={toggle}/>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#sop">SOP</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#asisten">Asisten</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#contact">Contact</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <NavbarText>React Js version : {React.version}</NavbarText>
+                </Collapse>
+            </Container>
+        </Navbar>
+    )
+
+}
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <Jumbotron className="d-flex flex-column align-items-center justify-content-center">
+                    <h1>Laboratorium Komputasi FMIPA</h1>
+                    <h1>Universitas Sebelas Maret</h1>
+                </Jumbotron>
+            </header>
+        )
+    }
+}
+
+class AppMain extends Component {
+    render() {
+        return (
+            <main className="App-main">
+
+            </main>
+        )
+    }
+}
+
+class AppFooter extends Component {
+    render() {
+        return (
+            <footer className="App-footer">
+
+            </footer>
+        )
+    }
+}
+
+class AppRouter extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Switch>
+                    <Route path="/" component={MyApp} exact={true}/>
+                </Switch>
+            </React.Fragment>
+        )
+    }
+}
+
+Navbar.propTypes = {
+    light: PropTypes.bool,
+    dark: PropTypes.bool,
+    fixed: PropTypes.string,
+    color: PropTypes.string,
+    role: PropTypes.string,
+    expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+    // pass in custom element to use
+}
+
+NavbarBrand.propTypes = {
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+    // pass in custom element to use
+}
+
+NavbarText.propTypes = {
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+    // pass in custom element to use
+}
+
+export default AppRouter;
